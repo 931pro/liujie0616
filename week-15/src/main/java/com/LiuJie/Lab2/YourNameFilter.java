5fb57c546dd8b36b90cb29459d096e7e91c54399
123 <2812154885@qq.com>
2022-06-05 22:28:22 +0800

    week 15

---
+++ week-15/src/main/java/com/LiuJie/Lab2/YourNameFilter.java
@@ -0,0 +1,22 @@
+package com.LiuJie.Lab2;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "YourNameFilter",urlPatterns = {"/Lab2/validate.jsp","/Lab2/welcome.jsp"})
+public class YourNameFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        System.out.println("your nameFilter -->before chain");
+        chain.doFilter(request, response);
+        System.out.println("your nameFilter -->after chain");
+    }
+}
